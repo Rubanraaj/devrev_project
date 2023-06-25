ceabc8297fcc7b367e84333e8812fd00337e9468
Rubanraaj <rubanraaj14@gmail.com>
2023-06-25 23:16:18 +0530

    Update URL pattern

--- flightbooking/flight/urls.py
+++ flightbooking/flight/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 namespace = 'flight'
 urlpatterns = [
-    path('user/signup/',views.signup,name='signup'),
-    path('flight/list/',views.flightList,name='FlightList'),
-    path('flight/book/<int:flight_id>', views.bookings, name ='Booking'),
-    path('bookings/list/', views.bookingList, name='BookingList'),
-    path('',views.user_login, name='user_login'),
+    path('', views.user_login, name='user_login'),
+    path('user/signup/', views.signup, name='signup'),
+    path('selection/', views.selection, name='selection'),
     path('search-flights/', views.search_flights, name='search_flights'),
-    path('selection/',views.selection,name='selection'),
-    path('logout/',views.user_logout,name='user_logout')
+    path('bookings/list/', views.bookingList, name='BookingList'),
+    path('flight/list/', views.flightList, name='FlightList'),
+    path('flight/book/<int:flight_id>', views.bookings, name ='Booking'),
+    path('logout/', views.user_logout, name='user_logout')
 
 ]
